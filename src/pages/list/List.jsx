2dbee3d1a2dbda1587872aa809ced1473d9e3590
Eka Septyaningrum <ekas72137@gmail.com>
2024-05-03 12:57:26 +0700

    first commit

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,9 +1,18 @@
 import "./list.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import Datatable from "../../components/datatable/Datatable";
 
-const Home = () => {
-    return (
-        <div>List</div>
-    )
-}
+const List = () => {
+  return (
+    <div className="list">
+      <Sidebar />
+      <div className="listContainer">
+        <Navbar />
+        <Datatable />
+      </div>
+    </div>
+  );
+};
 
-export default Home 
+export default List;
