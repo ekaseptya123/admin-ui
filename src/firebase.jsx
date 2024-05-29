9417acb615ac753f8a775871cd993bb54c34ed37
Eka Septyaningrum <ekas72137@gmail.com>
2024-05-29 19:53:40 +0700

    first commit

---
+++ src/firebase.jsx
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: "admin-ui-b3ffe.firebaseapp.com",
+  projectId: "admin-ui-b3ffe",
+  storageBucket: "admin-ui-b3ffe.appspot.com",
+  messagingSenderId: "709775488202",
+  appId: "1:709775488202:web:8414666b019c3c27a57033"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
